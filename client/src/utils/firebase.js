464179cfaad05c23c625643526b25f2ccab119ec
Aman Verma <aman@mac224.local>
2024-08-11 23:19:27 +0530

    first

---
+++ client/src/utils/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBMJmKpLzbi0KvEoxouyD_x_JmCbFtUyT0",
+  authDomain: "task-manager-a888f.firebaseapp.com",
+  projectId: "task-manager-a888f",
+  storageBucket: "task-manager-a888f.appspot.com",
+  messagingSenderId: "513953319194",
+  appId: "1:513953319194:web:d4f7c9ca8bebd346ce04a3",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
